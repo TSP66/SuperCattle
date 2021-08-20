22b53dbef410fc7e7bb3ad5b80673fdbde417af2
TSP66 <62830753+TSP66@users.noreply.github.com>
2021-08-20 21:59:45 +1000

    Continue on exmaple_grid

--- Programs/example_grid.py
+++ Programs/example_grid.py
@@ -20,6 +20,9 @@ for x in range(int(width/size)-1):
         window.create_rectangle(x*size, y*size, (x+1)*size, (y+1)*size, fill=colour, outline = colour)
 
 window.create_text(450,325,fill='black',font="Arial 30 italic bold",text="• Current Location")
+window.create_text(800,725,fill='black',font="Arial 30 italic bold",text="• Dam")
+window.create_text(550,525,fill='black',font="Arial 30 italic bold",text="• Dam")
+window.create_text(775,225,fill='black',font="Arial 30 italic bold",text="• Station")
 
 window.pack()
 
